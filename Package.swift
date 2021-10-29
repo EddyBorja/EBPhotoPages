c899ade2db335ec1680585abb89a3166cf6a1574
Marco Ricca <92715813+m-ricca@users.noreply.github.com>
2021-10-29 16:05:27 +0200

    updated SPM Package Descriptor

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     targets: [
         .target(
             name: "EBPhotoPagesController",
-            path: "EBPhotoPagesController"
+            path: "EBPhotoPagesController",
             publicHeadersPath: "./"
         )
     ]
